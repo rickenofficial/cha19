fce144ea8c0b1bbcb2c15b6d75fe1ad14236c821
rickenofficial <70503436+rickenofficial@users.noreply.github.com>
2024-09-08 15:49:03 -0600

    finishing the config in the file src-sw.js, install me button

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,36 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-// Add content to the database
 export const putDb = async (content) => {
   console.log('Post to the database');
-  // Create a connection to the database
+  //creates connection to db and version we want to use
   const jateDb = await openDB('jate', 1);
   // Create a new transaction and specify the database and data privileges
   const tx = jateDb.transaction('jate', 'readwrite');
-  // Open the object store
+  // opens up needed object store
   const store = tx.objectStore('jate');
-  // Store the content
-  const request = store.put({ value: content });
-  // Confirm the request
+  // stores the content
+  const request = store.put({ id: 1, value: content })
+  // Confirmation of request
   const result = await request;
   console.log('Data saved to the database', result);
 }
 
-// Get all content from the database
+// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('Get from the database');
-  // Create a connection to the database
+  // creates connection to db and version we want to use
   const jateDb = await openDB('jate', 1);
   // Create a new transaction and specify the database and data privileges
   const tx = jateDb.transaction('jate', 'readonly');
-  // Open the object store
+  // opens up needed object store
   const store = tx.objectStore('jate');
-  // Get all data from the object store
+  // gets all data in the database
   const request = store.getAll();
-  // Confirm the request
+   // Confirmation of request.
   const result = await request;
-  console.log('Content retrieved from the database', result);
-  return result;
+  console.log('result.value', result);
+  return result.value;
 }
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -19,11 +19,27 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html'
+        template: './index.html',
+        title: 'JATE',
       }),
+
+      
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
+
+      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+    
       new WebpackPwaManifest({
-        name: 'Just Another Text Editor',
+        fingerprints: false,
+        inject: true,
+        name: 'JATE Just Another Text Editor',
         short_name: 'JATE',
         description: 'A text editor with PWA capabilities.',
         background_color: '#225ca3',
@@ -33,37 +49,35 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [48, 72, 96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons')
-          }
-        ]
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+
+        ],
       }),
     ],
 
     module: {
-      rules: [{
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
           },
         },
-      },
       ],
     },
   };
